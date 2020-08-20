5492f21ea7e5b9ac66b6c76a364228591444976d
Edward Harman <eharman@tripadvisor.com>
2020-08-20 08:52:52 -0400

    docker is working

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Minecraft Pack Installer
 
-_Work In Progress - Come back later!_
-
 * For Minecraft Bedrock AKA MCPE, MCBE
 * Find addons/packs you have downloaded
 * Help reformat and install them
--- src/main/resources/default_config.groovy
+++ src/main/resources/default_config.groovy
@@ -1,3 +1,4 @@
 target "$mcpelauncher/games/com.mojang" // Linux or Mac OS using mcpelauncher
-target "/opt/MC/bedrock" // Linux server using MCscripts
+// TODO needs more info on target subdirs // target "/opt/MC/bedrock" // Linux server using MCscripts
 source "$HOME/Downloads"
+target "$HOME/Library/Application Support/mcpelauncher/games/com.mojang" // docker workaround / Mac OS
